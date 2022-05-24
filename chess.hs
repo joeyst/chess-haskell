989aec7861a51117c5bef2438dd12d68b6a369e7
joeyst <joeystenbeck@gmail.com>
2022-05-23 20:37:00 -0700

    Add getLetter, convertCase, and getPrintable.

--- chess.hs
+++ chess.hs
@@ -1,14 +1,18 @@
+import Data.Char
+
 data PieceType = 
   Pawn | 
   Rook |
   Knight |
   Bishop |
   Queen |
-  King deriving (Show, Eq)
+  King |
+  Empty deriving (Show, Eq)
 
 data Team =
   Black |
-  White deriving (Show, Eq)
+  White |
+  Blank deriving (Show, Eq)
 
 data Piece = Piece {
   typeOf :: PieceType,
@@ -21,13 +25,34 @@ type Rank = Integer
 data Square = Square {
   file :: File,
   rank :: Rank,
-  piece :: Maybe Piece
+  piece :: Piece
 } deriving (Show, Eq)
+
+getLetter :: Piece -> Char
+getLetter piece
+  | typeOf piece == Pawn = 'p'
+  | typeOf piece == Rook = 'r'
+  | typeOf piece == Knight = 'n'
+  | typeOf piece == Bishop = 'b'
+  | typeOf piece == Queen = 'q'
+  | typeOf piece == King = 'k'
+  | typeOf piece == Empty = ' '
+
+convertCase :: Piece -> Char -> Char
+convertCase piece ch
+  | teamOf piece == White = toUpper ch
+  | teamOf piece == Black = toLower ch
+  | teamOf piece == Blank = ch
+
+getPrintable :: Piece -> Char
+getPrintable piece = convertCase piece $ getLetter piece
 
 type Board = [Square]
 genBoard :: Board
-genBoard = [Square file rank Nothing | file <- ['a'..'h'], rank <- [1..8]]
+genBoard = [Square file rank (Piece Empty Blank) | file <- ['a'..'h'], rank <- [1..8]]
 
 main :: IO ()
 main = do
-  print(genBoard)
+  putChar $ getPrintable (Piece Pawn Black)
+  putChar $ getPrintable (Piece Knight White)
+  putChar $ getPrintable (Piece Empty Blank)
